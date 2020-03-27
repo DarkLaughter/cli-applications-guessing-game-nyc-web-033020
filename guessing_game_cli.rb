87a9c832ba7c17d2ce098d156f3ec82d78ad40bf
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-27 23:22:21 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,21 @@
 # Code your solution here!
+def run_guessing_game
+  guess = ""
+  
+  puts = "Guess a number beteen 1 and 6"
+  guess = gets.chomp
+  random_num = rand(1..6)
+  
+  if guess == "exit"
+    puts "Goodbye!"
+  end 
+  
+  case guess
+ if guess == random_num
+    puts "You guessed the correct number!"
+  else 
+    puts "Sorry! The computer guessed #{random_num}."
+
+  end
+  
+end 
